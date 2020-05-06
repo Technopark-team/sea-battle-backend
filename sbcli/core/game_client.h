ca4b433ca851987dc49af69ad7f658902feb722f
rufa-a <roofinda@gmail.com>
2020-05-06 23:12:29 +0300

    Init work on project arch

---
+++ sbcli/core/game_client.h
@@ -0,0 +1,12 @@
+#ifndef SEA_BATTLE_BACKEND_GAME_CLIENT_H
+#define SEA_BATTLE_BACKEND_GAME_CLIENT_H
+
+namespace seabattle {
+namespace client {
+namespace game {
+
+}
+}
+}
+
+#endif //SEA_BATTLE_BACKEND_GAME_CLIENT_H

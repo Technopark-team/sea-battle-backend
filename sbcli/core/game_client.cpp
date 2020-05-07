7a2ebb2547ed47374abc5533bff229040951a9e9
Rufina <roofinda@gmail.com>
2020-05-08 01:34:11 +0300

    main class game_client

--- sbcli/core/game_client.cpp
+++ sbcli/core/game_client.cpp
@@ -1,1 +1,31 @@
 #include "game_client.h"
+#include <iostream>
+
+namespace seabattle {
+namespace client {
+namespace game {
+
+GameClient::GameClient()
+    : network_client_(new network::NetworkClient()),
+      menu_controller_(new controller::MenuController()) {
+        auth_controller_ = std::unique_ptr<controller::AuthController>(new controller::AuthController(network_client_));
+
+}
+size_t GameClient::run() {
+    config::UserCommand command;
+    config::UserData user_data;
+    while (command.command.compare("exit") != 0) {  // пока не выход
+        auth_controller_->GetUserId(user_data);
+        if (user_data.user_id == 0) {
+            auth_controller_->Action(command);
+        }
+        if (command.command.compare("menu") == 0) {
+            menu_controller_->Action(command);
+        }
+    }
+    std::cout << "Ура, мы вышли" << std::endl;
+    return 0;
+}
+}  // namespace game
+}  // namespace client
+}  // namespace seabattle
--- sbcli/core/game_client.h
+++ sbcli/core/game_client.h
@@ -1,12 +1,32 @@
 #ifndef SEA_BATTLE_BACKEND_GAME_CLIENT_H
 #define SEA_BATTLE_BACKEND_GAME_CLIENT_H
 
+#include <cstddef>
+#include <memory>
+#include "config/config.h"
+#include "controller/auth.h"
+#include "controller/menu.h"
+#include "model/user_model.h"
+#include "network/network.h"
+#include "ui/auth_console_input.h"
+
 namespace seabattle {
 namespace client {
 namespace game {
 
-}
-}
-}
+class GameClient {
+ public:
+    GameClient();
+    size_t run();
 
-#endif //SEA_BATTLE_BACKEND_GAME_CLIENT_H
+ private:
+    std::unique_ptr<controller::AuthController> auth_controller_;
+    std::unique_ptr<controller::MenuController> menu_controller_;
+    std::shared_ptr<network::INetworkClient> network_client_;
+};
+
+}  // namespace game
+}  // namespace client
+}  // namespace seabattle
+
+#endif  // SEA_BATTLE_BACKEND_GAME_CLIENT_H

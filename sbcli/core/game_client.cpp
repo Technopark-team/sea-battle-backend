54ce1b4290e6c48039db0f060f98f43e95e8b2b2
Rufina <roofinda@gmail.com>
2020-05-13 19:37:44 +0300

    added command types

--- sbcli/core/game_client.cpp
+++ sbcli/core/game_client.cpp
@@ -12,16 +12,30 @@ GameClient::GameClient() : menu_controller_(new controller::MenuController()) {
 }
 size_t GameClient::run() {
     // TODO: возвращать значение команды без создания переменной в этой функции
-    config::UserCommand command;
+    config::UserCommand user_command;
     // TODO: возвращать bool значение для статуса авторизированного пользователя без переменной
     utils::data::UserData user_data;
-    while (command.command.compare("exit") != 0) {  // пока не выход
+    while (true) {  // пока не выход
         auth_controller_->GetUserData(user_data);
         if (user_data.user_id == -1) {
-            auth_controller_->Action(command);
+            auth_controller_->Action(user_command);
         }
-        if (command.command.compare("menu") == 0) {
-            menu_controller_->Action(command);
+        switch (user_command.command){
+            case config::EXIT_COMMAND:
+                break;
+            case config::MENU_COMMAND:
+                menu_controller_->Action(user_command);
+                break;
+            case config::SINGLE_COMMAND:
+                break;
+            case config::LOAD_COMMAND:
+                break;
+            case config::MULTI_COMMAND:
+                break;
+        }
+
+        if (user_command.command == config::EXIT_COMMAND) {
+            break;
         }
     }
     std::cout << "Ура, мы вышли" << std::endl;

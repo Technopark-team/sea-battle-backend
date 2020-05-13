3ead7da4f9b743a9ff97912464974e1f5e3ebadf
Rufina <roofinda@gmail.com>
2020-05-13 15:42:51 +0300

    serialization with msgpack library

--- sbutils/serialization/serializer.h
+++ sbutils/serialization/serializer.h
@@ -1,4 +1,49 @@
 #ifndef SEA_BATTLE_BACKEND_SERIALIZER_H
 #define SEA_BATTLE_BACKEND_SERIALIZER_H
 
-#endif //SEA_BATTLE_BACKEND_SERIALIZER_H
+#include <iostream>
+#include <memory>
+#include "sbutils/data/data.h"
+
+namespace seabattle {
+namespace utils {
+namespace serializer {
+
+template <class T>
+class Serializer {
+ public:
+    explicit Serializer();
+    ~Serializer() = default;
+
+    static std::shared_ptr<std::stringstream> Serialize(T& data);
+
+    static std::shared_ptr<T> Deserialize(std::stringstream& stream, size_t bytes);
+};
+
+template <class T>
+Serializer<T>::Serializer() {}
+
+// TODO: сделать статическими
+// TODO: вынести в файл cpp
+
+template <class T>
+std::shared_ptr<std::stringstream> Serializer<T>::Serialize(T& data) {
+    std::shared_ptr<std::stringstream> ss = std::make_shared<std::stringstream>();
+    msgpack::pack(*ss, data);
+    return ss;
+}
+
+template <class T>
+std::shared_ptr<T> Serializer<T>::Deserialize(std::stringstream& stream, size_t bytes) {
+    std::shared_ptr<T> data = std::make_shared<T>();
+    msgpack::object_handle oh = msgpack::unpack(stream.str().data(), bytes);
+    msgpack::object obj = oh.get();
+    obj.convert(*data);
+    return data;
+}
+
+}  // namespace serializer
+}  // namespace utils
+}  // namespace seabattle
+
+#endif  // SEA_BATTLE_BACKEND_SERIALIZER_H

4f87227ea126123439ff754b8c8161472fdf881e
rufa-a <roofinda@gmail.com>
2020-05-20 17:38:49 +0300

    action function with controller_signal, added writing to auth_data

--- sbcli/controller/auth.cpp
+++ sbcli/controller/auth.cpp
@@ -1,5 +1,8 @@
 #include "auth.h"
 
+#include <chrono>
+#include <thread>
+
 namespace seabattle {
 namespace client {
 namespace controller {
@@ -8,26 +11,48 @@ AuthController::AuthController(std::shared_ptr<network::TCPClient>& network_clie
     : user_model_(new model::UserModel(network_client)),
       console_interface_(new ui::AuthConsoleInput()) {}
 
-size_t AuthController::Action(config::UserCommand& user_command) {
-    config::UserCommand auth_command;
+size_t AuthController::Action(std::shared_ptr<config::ControllerSignal>& controller_signal) {
+    size_t auth_status = controller_signal->auth_user_status;
+    controller_signal->Clean();
+    size_t command;
     utils::data::AuthData auth_data;
+    if (controller_signal->auth_user_status == config::UserStatus::AUTHORIZED) {
+        console_interface_->RenderAuth(command);
+        if (command == 0) {
+            // TODO: сделать logout в модели и на сервере
+        }
+    } else {
+        console_interface_->RenderNonAuth(command);
+        if (command == 0) {
+            console_interface_->ReadAuthData(auth_data);
+            // TODO: провалидировать auth_data
+            user_model_->SetAuthData(auth_data);
 
-    console_interface_->ReadCommand(auth_command);
+            // TODO: вызвать PostSignin()/PostSignup() в зависимости от команды авторизации или
+            // регистрации
+            // TODO: это неблокирующая операция, требуется синхронизировать асинхронную операцию
+            user_model_->PostSignin();
+            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+            GetUserData(controller_signal->user_data);
+            controller_signal->auth_user_status = config::UserStatus::AUTHORIZED;
+        }
+    }
+    controller_signal->signal = config::Controller::MENU;
+    //    config::UserCommand auth_command;
+    //    utils::data::AuthData auth_data;
+    //
+    //    console_interface_->ReadCommand(auth_command);
+    //
+    //    // TODO: проверить, что введена команда авторизации или регистрации
+    //    console_interface_->ReadAuthData(auth_data);
+    //
 
-    // TODO: проверить, что введена команда авторизации или регистрации
-    console_interface_->ReadAuthData(auth_data);
-
-    // TODO: провалидировать auth_data
-    user_model_->SetAuthData(auth_data);
-
-    // TODO: вызвать PostSignin()/PostSignup() в зависимости от команды авторизации или регистрации
-    // TODO: это блокирующая операция, требуется продумать корректный коллбек
-    user_model_->PostSignin();
-
-    // TODO: продумать view, которая рендерит успех/неудачу авторизации/регистрации
-    // TODO: предложение пользователю войти в меню или выйти из игры только после успеха
-    // TODO: вынести из контроллера авторизации, так как не его поле деятельности
-    console_interface_->ReadCommand(user_command);
+    //
+    //    // TODO: продумать view, которая рендерит успех/неудачу авторизации/регистрации
+    //    // TODO: предложение пользователю войти в меню или выйти из игры только после успеха
+    //    // TODO: вынести из контроллера авторизации, так как не его поле деятельности
+    ////    console_interface_->ReadCommand(user_command);
+    //    controller_signal->signal = config::Controller::NONE;
     return 0;
 }
 

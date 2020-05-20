3b0184e344ac1cc79638ee922b096ae4118f4789
rufa-a <roofinda@gmail.com>
2020-05-20 17:51:06 +0300

    added controller_signal for controllers interaction with using Controller enum

--- sbcli/core/game_client.cpp
+++ sbcli/core/game_client.cpp
@@ -9,54 +9,26 @@ namespace game {
 GameClient::GameClient() : menu_controller_(new controller::MenuController()) {
     network_client_ = std::make_shared<network::TCPClient>(ioc);
     auth_controller_ = std::make_unique<controller::AuthController>(network_client_);
+    controller_signal_ = std::make_shared<config::ControllerSignal>(config::Controller::MENU);
 }
-size_t GameClient::run() {
-    /* TODO: возвращать значение команды без создания переменной в этой функции
-     * Требуется определиться с логикой контроллеров:
-     *
-     * Может быть, создать сигналы переходов между контроллерами (сигнал соответсвует контроллеру)
-     * а также статус пользователя:
-     *      enum ControllerSignal {
-     *          MENU = 1,
-     *          AUTH,
-     *          GAME,
-     *      }
-     *      enum UserStatus {
-     *          EXIST = 1,
-     *          NONEXIST,
-     *      }
-     *      size_t user_status_ = 0;
-     */
-    config::UserCommand user_command;
-    // TODO: возвращать bool значение для статуса авторизированного пользователя без переменной (outdated: нам нужны данные по пользователю для игровой модели)
-    utils::data::UserData user_data;
-    while (user_command.command != config::CLOSE_COMMAND) {  // пока не закрытие
-        auth_controller_->GetUserData(user_data);
-        if (user_data.user_id == -1) {
-            auth_controller_->Action(user_command);
-        }
-        switch (user_command.command){
-            case config::CLOSE_COMMAND:
-                break;
-            case config::MENU_COMMAND:
-                menu_controller_->Action(user_command);
-                break;
-            case config::SINGLE_COMMAND:
-                break;
-            case config::LOAD_COMMAND:
-                break;
-            case config::MULTI_COMMAND:
-                break;
-        }
 
-        if (user_command.command == config::CLOSE_COMMAND) {
-            break;
+GameClient::~GameClient() { network_client_->Close(); }
+
+size_t GameClient::run() {
+    while (controller_signal_->signal != config::Controller::NONE) {  // пока не закрытие
+        switch (controller_signal_->signal) {
+            case config::Controller::AUTH:
+                auth_controller_->Action(controller_signal_);
+                break;
+            case config::Controller::MENU:
+                menu_controller_->Action(controller_signal_);
+                break;
+            case config::Controller::GAME:
+                game_controller_->Action(controller_signal_);
+                break;
         }
     }
     std::cout << "Ура, мы вышли" << std::endl;
-
-    // TODO: перенести в деструктор
-    network_client_->Close();
 
     return 0;
 }

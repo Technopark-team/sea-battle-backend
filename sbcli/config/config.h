05aeab6c111d96942e5c29b4d0a2d19f8aa07736
rufa-a <roofinda@gmail.com>
2020-05-20 17:21:40 +0300

    added struct controller_signal and enum commands and auth_status to connect process of controllers

--- sbcli/config/config.h
+++ sbcli/config/config.h
@@ -2,6 +2,7 @@
 #define SEA_BATTLE_BACKEND_CONFIG_H
 
 #include <string>
+#include "sbutils/data/data.h"
 
 namespace seabattle {
 namespace client {
@@ -10,14 +11,27 @@ namespace config {
 // TODO: добавить струтуру ip/port сервера
 
 enum UserCommandId {
-    CLOSE_COMMAND = 1,
-    SIGNIN_COMMAND,
+    SIGNIN_COMMAND = 1,
     SIGNUP_COMMAND,
     MENU_COMMAND,
+    LOGOUT_COMMAND,
+    CLOSE_COMMAND,
     SINGLE_COMMAND,
     LOAD_COMMAND,
     MULTI_COMMAND,
     STEP_COMMAND,
+};
+
+enum UserStatus {
+    AUTHORIZED = 1,
+    NOT_AUTHORIZED,
+};
+
+enum Controller {
+    AUTH = 1,
+    MENU,
+    GAME,
+    NONE,
 };
 
 struct UserCommand {
@@ -25,6 +39,27 @@ struct UserCommand {
 
     UserCommand() = default;
     UserCommand(size_t com) : command(com) {}
+};
+
+struct ControllerSignal {
+    size_t signal = -1;
+    size_t auth_user_status = -1;
+    utils::data::UserData user_data = utils::data::UserData();
+    UserCommand command = UserCommand();
+
+    ControllerSignal() = default;
+    ControllerSignal(size_t signal) : signal(signal) {}
+    ControllerSignal(size_t signal, size_t auth_user_status, utils::data::UserData user_data,
+                     UserCommand command)
+        : signal(signal),
+          auth_user_status(auth_user_status),
+          user_data(std::move(user_data)),
+          command(std::move(command)) {}
+
+    void Clean() {
+        signal = -1;
+        command = UserCommand();
+    }
 };
 
 }  // namespace config

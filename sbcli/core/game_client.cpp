8ae97e94c540e0b3ad43b42619037558a509feb0
rufa-a <roofinda@gmail.com>
2020-05-27 14:00:11 +0300

    added game_controller and right close

--- sbcli/core/game_client.cpp
+++ sbcli/core/game_client.cpp
@@ -9,10 +9,17 @@ namespace game {
 GameClient::GameClient() : menu_controller_(new controller::MenuController()) {
     network_client_ = std::make_shared<network::TCPClient>(ioc);
     auth_controller_ = std::make_unique<controller::AuthController>(network_client_);
+    game_controller_ = std::make_unique<controller::GameController>(network_client_);
     controller_signal_ = std::make_shared<config::ControllerSignal>(config::Controller::MENU);
+
+    setlocale(LC_ALL, "");
+    initscr();
 }
 
-GameClient::~GameClient() { network_client_->Close(); }
+GameClient::~GameClient() {
+    network_client_->Close();
+    endwin();
+}
 
 size_t GameClient::run() {
     while (controller_signal_->signal != config::Controller::NONE) {  // пока не закрытие

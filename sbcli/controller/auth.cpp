a6a2e06f012d79d2fb741b7d834e711ab9ddb786
rufa-a <roofinda@gmail.com>
2020-05-27 13:58:42 +0300

    changed size_t to enum classes types, added switch-cases for new structs

--- sbcli/controller/auth.cpp
+++ sbcli/controller/auth.cpp
@@ -13,12 +13,14 @@ AuthController::AuthController(std::shared_ptr<network::TCPClient>& network_clie
 
 size_t AuthController::Action(std::shared_ptr<config::ControllerSignal>& controller_signal) {
     size_t command = -1;
-    size_t auth_status = controller_signal->auth_user_status;
+    config::UserStatus auth_status = controller_signal->auth_user_status;
+    config::UserCommand menu_command = controller_signal->command;
     controller_signal->Clean();
     utils::data::TestAuthData auth_data;
 
     console_interface_->Run(command, auth_data, auth_status);
 
+    // TODO: переписать эти условные конструкции
     if (controller_signal->auth_user_status == config::UserStatus::AUTHORIZED) {
         if (command == 0) {
             // TODO: сделать logout в модели и на сервере
@@ -28,20 +30,27 @@ size_t AuthController::Action(std::shared_ptr<config::ControllerSignal>& control
             // TODO: провалидировать auth_data
             user_model_->SetAuthData(auth_data);
 
-            // TODO: вызвать PostSignin()/PostSignup() в зависимости от команды авторизации или
-            // регистрации
+            // TODO: вызвать PostSignin()/PostSignup() в зависимости от команды авторизации или регистрации
             // TODO: это неблокирующая операция, требуется синхронизировать асинхронную операцию
-            user_model_->CreateUser();
-            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+            switch (menu_command.command) {
+                case config::UserCommandId::SIGNIN_COMMAND:
+                    user_model_->Enter();
+                    break;
+                case config::UserCommandId::SIGNUP_COMMAND:
+                    user_model_->CreateUser();
+                    break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             GetUserData(controller_signal->user_id);
             controller_signal->auth_user_status = config::UserStatus::AUTHORIZED;
         }
     }
+    // command == 1 - возвращение в меню
     controller_signal->signal = config::Controller::MENU;
     return 0;
 }
 
-size_t AuthController::GetUserData(int &user_id) {
+size_t AuthController::GetUserData(int& user_id) {
     user_model_->GetUserData(user_id);
     return 0;
 }

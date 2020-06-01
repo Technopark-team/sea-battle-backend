9eb9b468e0036253438261341fe8806ee6720a96
rufa-a <roofinda@gmail.com>
2020-06-01 07:32:08 +0300

    improved game client code

--- sbcli/core/game_client.cpp
+++ sbcli/core/game_client.cpp
@@ -6,49 +6,64 @@ namespace seabattle {
 namespace client {
 namespace game {
 
-GameClient::GameClient(config::IpPort net_config, config::Debug debug) : menu_controller_(new controller::MenuController()) {
-    network_client_ = std::make_shared<network::TCPClient>(ioc, net_config);
+GameClient::GameClient(config::IpPort net_config, config::Debug debug)
+    : net_config_(std::move(net_config)),
+      debug_data_(std::move(debug)),
+      menu_controller_(new controller::MenuController()) {
+    std::cout << "[GameClient] GameClient" << std::endl;
+    network_client_ = std::make_shared<network::TCPClient>(net_config_);
+    test_client_ = std::make_unique<test::TestClient>(debug, network_client_);
     auth_controller_ = std::make_unique<controller::AuthController>(network_client_);
     game_controller_ = std::make_unique<controller::GameController>(network_client_);
     controller_signal_ = std::make_shared<config::ControllerSignal>(config::Controller::MENU);
-    controller_signal_->debug = std::move(debug);
-    setlocale(LC_ALL, "");
 
-    if (controller_signal_->debug.dev_mode == config::DevMode::RELEASE) {
-        initscr();
-
-        if(has_colors() == FALSE)
-        {	endwin();
-            printf("Your terminal does not support color\n");
-            exit(1);
-        }
-        start_color();
+    if (debug_data_.dev_mode == config::DevMode::RELEASE) {
+        InitGraphics();
     }
-
 }
 
 GameClient::~GameClient() {
-    network_client_->Close();
-    if (controller_signal_->debug.dev_mode == config::DevMode::RELEASE) {
+    std::cout << "[GameClient] ~GameClient" << std::endl;
+//    network_client_->Close();
+    if (debug_data_.dev_mode == config::DevMode::RELEASE) {
         endwin();
     }
 }
 
-size_t GameClient::run() {
-    while (controller_signal_->signal != config::Controller::NONE) {  // пока не закрытие
-        switch (controller_signal_->signal) {
-            case config::Controller::AUTH:
-                auth_controller_->Action(controller_signal_);
-                break;
-            case config::Controller::MENU:
-                menu_controller_->Action(controller_signal_);
-                break;
-            case config::Controller::GAME:
-                game_controller_->Action(controller_signal_);
-                break;
+size_t GameClient::InitGraphics() {
+    std::cout << "[GameClient] InitGraphics" << std::endl;
+    setlocale(LC_ALL, "");
+
+    initscr();
+
+    if (has_colors() == FALSE) {
+        endwin();
+        std::cout << "[GameClient] Your terminal does not support color\n" << std::endl;
+        exit(1);
+    }
+    start_color();
+}
+
+size_t GameClient::Run() {
+    std::cout << "[GameClient] Run" << std::endl;
+    if (debug_data_.dev_mode == config::DevMode::DEV) {
+        test_client_->Run();
+    } else if (debug_data_.dev_mode == config::DevMode::RELEASE) {
+        while (controller_signal_->signal != config::Controller::NONE) {  // пока не закрытие
+            switch (controller_signal_->signal) {
+                case config::Controller::AUTH:
+                    auth_controller_->Action(controller_signal_);
+                    break;
+                case config::Controller::MENU:
+                    menu_controller_->Action(controller_signal_);
+                    break;
+                case config::Controller::GAME:
+                    game_controller_->Action(controller_signal_);
+                    break;
+            }
         }
     }
-    std::cout << "Ура, мы вышли" << std::endl;
+    std::cout << "[GameClient] Ура, мы вышли" << std::endl;
 
     return 0;
 }
--- sbcli/core/game_client.h
+++ sbcli/core/game_client.h
@@ -1,14 +1,15 @@
 #ifndef SEA_BATTLE_BACKEND_GAME_CLIENT_H
 #define SEA_BATTLE_BACKEND_GAME_CLIENT_H
 
+#include <locale.h>
 #include <cstddef>
 #include <memory>
 #include <string>
-#include <locale.h>
-#include "config/config.h"
-#include "controller/auth.h"
-#include "controller/menu.h"
-#include "controller/game.h"
+#include "sbcli/config/config.h"
+#include "sbcli/controller/auth.h"
+#include "sbcli/controller/game.h"
+#include "sbcli/controller/menu.h"
+#include "sbcli/core/test_client.h"
 
 // TODO: добавить документацию к методам и свойствам
 
@@ -16,21 +17,26 @@ namespace seabattle {
 namespace client {
 namespace game {
 
-namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace net = boost::asio;  // from <boost/asio.hpp>
 
 class GameClient {
  public:
     GameClient(config::IpPort net_config, config::Debug debug);
     ~GameClient();
-    size_t run();
+    size_t Run();
 
  private:
+    size_t InitGraphics();
+
+    std::unique_ptr<test::TestClient> test_client_;
     std::unique_ptr<controller::AuthController> auth_controller_;
     std::unique_ptr<controller::MenuController> menu_controller_;
     std::unique_ptr<controller::GameController> game_controller_;
     std::shared_ptr<network::TCPClient> network_client_;
     std::shared_ptr<config::ControllerSignal> controller_signal_;
-    net::io_service ioc;
+
+    config::Debug debug_data_;
+    config::IpPort net_config_;
 };
 
 }  // namespace game
